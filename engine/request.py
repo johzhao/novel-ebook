ca0498489a9bf8017aa6cd10c81c95f3b035b023
john.zhao <john.zhao@graviti.cn>
2020-10-15 16:24:00 +0800

    implement task queue by using redis

--- engine/request.py
+++ engine/request.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 logger = logging.getLogger(__name__)
@@ -7,4 +8,13 @@ logger.addHandler(logging.NullHandler())
 class Request:
 
     def __init__(self):
-        pass
+        self.url = ''
+        self.headers = {}
+
+    def serialize(self) -> str:
+        obj = vars(self)
+        return json.dumps(obj, ensure_ascii=False, sort_keys=True)
+
+    def deserialize(self, data: str):
+        obj = json.loads(data)
+        self.__dict__.update(obj)
--- engine/task_queue.py
+++ engine/task_queue.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Optional
+import redis
 
 from .request import Request
 
@@ -8,12 +9,32 @@ logger.addHandler(logging.NullHandler())
 
 
 class TaskQueue:
+    PENDING_TASK_KEY = 'pending_tasks'
+    FINISHED_TASK_KEY = 'finished_tasks'
+    FAILED_TASK_KEY = 'failed_tasks'
 
     def __init__(self):
-        pass
+        # TODO: Pass the config by parameters
+        self._pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+        self._redis = redis.Redis(connection_pool=self._pool)
 
-    def push_task(self, request: Request):
-        pass
+    def append_pending_task(self, request: Request):
+        with self._redis:
+            self._redis.lpush(self.PENDING_TASK_KEY, request.serialize())
 
-    def pop_task(self) -> Optional[Request]:
-        pass
+    def append_finished_task(self, request: Request):
+        with self._redis:
+            self._redis.lpush(self.FINISHED_TASK_KEY, request.serialize())
+
+    def append_failed_task(self, request: Request):
+        with self._redis:
+            self._redis.lpush(self.FAILED_TASK_KEY, request.serialize())
+
+    def get_pending_task(self) -> Optional[Request]:
+        with self._redis:
+            data = self._redis.lpop(self.PENDING_TASK_KEY)
+            if data:
+                req = Request()
+                req.deserialize(data)
+                return req
+            return None
